dfb2eb6025a5d255252feb7dc9d6f4a0744a3462
vsvsilva <vitor.ovitor@outlook.com>
2022-09-22 18:14:24 -0300

    armazenando dados na web

--- js/main.js
+++ js/main.js
@@ -1,11 +1,17 @@
 const formulario = document.getElementById("novoItem");
 const lista = document.getElementById("lista");
+const itens = [];
 
 formulario.addEventListener("submit", (evento) => {
   evento.preventDefault();
-  let nome = evento.target.elements["nome"].value;
-  let quantidade = evento.target.elements["quantidade"].value;
-  criaElemento(nome, quantidade);
+
+  const nome = evento.target.elements["nome"];
+  const quantidade = evento.target.elements["quantidade"];
+
+  criaElemento(nome.value, quantidade.value);
+
+  nome.value = "";
+  quantidade.value = "";
 });
 
 function criaElemento(nome, quantidade) {
@@ -17,6 +23,13 @@ function criaElemento(nome, quantidade) {
 
   novoItem.appendChild(numeroItem);
   novoItem.innerHTML += nome;
-
   lista.appendChild(novoItem);
+
+  const itemAtual = {
+    "nome": nome,
+    "quantidade": quantidade
+  }
+  itens.push(itemAtual);
+  localStorage.setItem("item", JSON.stringify(itens));
+
 };
