fad4eb93b459aa6217c21dca20793170825d1050
vsvsilva <vitor.ovitor@outlook.com>
2022-09-27 19:25:04 -0300

    Atualizando um item

--- js/main.js
+++ js/main.js
@@ -12,14 +12,23 @@ formulario.addEventListener("submit", (evento) => {
   const nome = evento.target.elements["nome"];
   const quantidade = evento.target.elements["quantidade"];
 
+  const existe = itens.find(elemento => elemento.nome === nome.value);
+
   const itemAtual = {
     "nome": nome.value,
     "quantidade": quantidade.value
   }
 
-  criaElemento(itemAtual);
+  if(existe) {
+    itemAtual.id = existe.id;
+    atualizaElemento(itemAtual);
+  }else {    
+    itemAtual.id = itens.length;
+    criaElemento(itemAtual);
+    
+    itens.push(itemAtual);
+  }
 
-  itens.push(itemAtual);
   localStorage.setItem("itens", JSON.stringify(itens));
 
   nome.value = "";
@@ -31,9 +40,16 @@ function criaElemento(item) {
   novoItem.classList.add("item");
 
   const numeroItem = document.createElement("strong");
+  numeroItem.dataset.id = item.id
   numeroItem.innerHTML = item.quantidade;
 
   novoItem.appendChild(numeroItem);
   novoItem.innerHTML += item.nome;
   lista.appendChild(novoItem);
+};
+
+function atualizaElemento(item) {
+  document.querySelector("[data-id='"+item.id+"']").innerHTML = item.quantidade;
+
+  itens[item.id] = item;
 };
